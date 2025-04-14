379b206329520ccb18168c09a079dbd31ab8aebc
sA_9o1at <crissandres28@gmail.com>
2025-04-14 14:37:14 -0500

    Mejora en el Ingreso de datos en decimales ,ademas se cambio una linea que se repetida y no era necesario

--- src/main/java/com/mycompany/practica_n2/Practica_N2.java
+++ src/main/java/com/mycompany/practica_n2/Practica_N2.java
@@ -10,94 +10,99 @@ public class Practica_N2 {
 
     public static void main(String[] args) {
 
-        Scanner teclado = new Scanner(System.in);
+        Scanner teclado = new  Scanner(System.in);
         System.out.println("Ingrese las 3 notas del Primer Ciclo.....");
-        float[] notas1 = new float[3];
-        for (int i = 0; i < 3; i++) {
-
-            System.out.println("Ingresa la nota N" + (i + 1));
-            notas1[i] = teclado.nextFloat();
+        float [ ] notas1 = new float[3];        
+        for(int i=0 ; i<3 ; i++){
+            
+            System.out.println("Ingresa la nota N"+ (i+1));
+            String entrada=teclado.nextLine();
+            notas1 [i]= Float.parseFloat(entrada.replace(",","."));
         }
         System.out.println("----------------------------------------");
 
-        for (int i = 0; i < 3; i++) {
+        
+        for(int i=0 ; i<3 ; i++){
             System.out.println("Estas son tus notas ingresadas:");
-            System.out.println("Nota N" + (i + 1) + ": " + notas1[i] + " ");
+        System.out.println("Nota N"+(i+1)+": "+notas1[i]+" ");
 
         }
         System.out.println("--------------------------------");
-        float suma1 = 0;
-        float promedio1 = 0;
-        for (int i = 0; i < 3; i++) {
-            suma1 = suma1 + notas1[i];
+        float suma1=0;
+        float promedio1=0;
+        for(int i=0; i<3;i++){
+        suma1= suma1 + notas1[i];
         }
 
-        promedio1 = suma1 / 3;
-        System.out.println("Tu nota del Primer Ciclo es :" + promedio1);
+        promedio1= suma1/3;
+        System.out.println("Tu nota del Primer Ciclo es :"+promedio1);
 
         System.out.println("--------------------------------");
         System.out.println("--------------------------------");
 
         System.out.println("Ingrese las 3 notas del Segundo Ciclo.....");
-        float[] notas2 = new float[3];
-        for (int i = 0; i < 3; i++) {
-
-            System.out.println("Ingresa la nota N" + (i + 1));
-            notas2[i] = teclado.nextFloat();
+        float [ ] notas2 = new float[3];        
+        for(int i=0 ; i<3 ; i++){
+            
+            System.out.println("Ingresa la nota N"+ (i+1));
+            notas2 [i]= teclado .nextFloat();
         }
         System.out.println("----------------------------------------");
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Estas son tus notas ingresadas:");
-            System.out.println("Nota N" + (i + 1) + ": " + notas2[i] + " ");
+        System.out.println("Estas son tus notas ingresadas:");
+        for(int i=0 ; i<3 ; i++){
+            
+        System.out.println("Nota N"+(i+1)+": "+notas2[i]+" ");
 
         }
         System.out.println("--------------------------------");
-        float suma2 = 0;
-        float promedio2 = 0;
-        for (int i = 0; i < 3; i++) {
-            suma2 = suma2 + notas2[i];
+        float suma2=0;
+        float promedio2=0;
+        for(int i=0; i<3;i++){
+        suma2= suma2 + notas2[i];
         }
-
-        promedio2 = suma2 / 3;
-        System.out.println("Tu nota del Segundo Ciclo es :" + promedio2);
+        
+        promedio2= suma2/3;
+        System.out.println("Tu nota del Segundo Ciclo es :"+promedio2);
 
         System.out.println("--------------------------------");
         System.out.println("--------------------------------");
-        float promedio = 0;
-        promedio = promedio1 + promedio2;
-        System.out.println("Tu promedio de los Tres Ciclos es :" + promedio);
+        float promedio=0;
+        promedio= promedio1+promedio2;
+        System.out.println("Tu promedio de los Tres Ciclos es :"+promedio);
 
-        if (promedio < 14) {
+        if(promedio < 14){
             System.out.println("No alcansas la nota permitida Reprueba");
             System.out.println("-----------------------------------");
             System.out.println("Para poder acceder al examen de recuperacion debes tener una nota permitda");
+            
+            float noPermitida = (promedio/2);
+            float notaFinal=noPermitida;
+            noPermitida = noPermitida+10;
 
-            float noPermitida = (promedio / 2);
-            float notaFinal = noPermitida;
-            noPermitida = noPermitida + 10;
-
-            float notaExam = 0;
-            if (noPermitida < 8) {
+            
+            if(noPermitida<8){
 
                 System.out.println("Con tu prodio actual no alcanzaras para aprobar la materia");
                 System.out.println("---------------------------");
-                System.out.println("Reprobado con tu promedio de " + promedio);
-            } else {
+                System.out.println("Reprobado con tu promedio de "+promedio);
+            }else{
                 System.out.println("Puedes dar el Examen de Recuperacion");
                 System.out.println("----------------------------");
                 System.out.println("Ingresa la nota del Examen dado: ");
-                notaExam = teclado.nextFloat();
-                notaFinal = notaFinal + notaExam;
-                if (notaFinal >= 14) {
+                float notaExam=0;
+                notaExam= teclado.nextFloat();
+                notaFinal= notaFinal+notaExam;
+                if (notaFinal>=14){
                     System.out.println("Felicidades APREUBA LA MATERIA ");
-                } else {
+                }else{
                     System.out.println("DESAPRUEBAS LA MATERIA");
                 }
             }
-
-        } else {
+            
+        }
+        else{
             System.out.println("Alcansas la nota permitida Aprueba");
         }
-    }
+}
 }
